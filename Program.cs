09206f8a565365e35bb6295dfb86cfacdb899bbe
Mazino <fredozv01@gmail.com>
2024-10-23 19:55:06 +0200

    fonction Rejouer

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Json;
 using System.Security.Authentication;
 using AsciiArt;
 
@@ -45,7 +46,7 @@ namespace jeu_du_pendu
             return false;
         }
 
-        static char DemanderUneLettre()
+        static char DemanderUneLettre(string message = "Rendrez une lettre : ")
         {
             // Rendrez une lettre
             // ERREUR : Vous devez rentrer une seule lettre
@@ -53,7 +54,7 @@ namespace jeu_du_pendu
 
             while (true)
             {
-                Console.Write("Rendrez une lettre : ");
+                Console.Write(message);
                 string reponse = Console.ReadLine();
                 if (reponse.Length == 1)
                 {
@@ -152,10 +153,40 @@ namespace jeu_du_pendu
             }
             return null;
         }
+        static bool DemenderRejouer()
+        {
+
+            
+            char reponse = DemanderUneLettre("Voulez-vous rejour (O / N )");
+            if (reponse == 'o' || reponse == 'O')
+            {
+                return true;
+
+
+            }
+
+            else if ((reponse == 'n') || (reponse == 'N'))
+            {
+
+                return false; // on sort 
+
+
+
+            }
+            else
+            {
+
+
+                Console.WriteLine("ERREUR");
+                return DemenderRejouer();
+
+            }
+
+        }
 
         static void Main(string[] args)
         {
-            Random rand = new Random();
+
             var mots = ChargerLesMots("mots.txt");
 
             if ((mots == null) || (mots.Length == 0))
@@ -165,13 +196,27 @@ namespace jeu_du_pendu
             }
             else
             {
-                string mot = mots[0].Trim().ToUpper(); //efface les espace et convertir en majusculle 
 
-                int i = rand.Next(mots.Length);
-                DevinerMot(mots[i].Trim().ToUpper() ) ;
+                while (true)
+                {
+                    Console.Clear();
+                    Random rand = new Random();
+                    string mot = mots[0].Trim().ToUpper(); //efface les espace et convertir en majusculle 
 
+                    int i = rand.Next(mots.Length);
+                    DevinerMot(mots[i].Trim().ToUpper());
 
+                    if (!DemenderRejouer()) // si je ne veux pas rejouer 
+                    {
+
+                        break;
+                    }
+
+                }
+
+                Console.WriteLine("AUREVOIR !!!");
             }
+
         }
     }
 }
